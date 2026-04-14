0d23a6a260db2eee2ecb467415840f9249294497
Mazeraser <danila.solowyow2016@yandex.ru>
2026-04-14 21:49:47 +0300

    all code

--- GraphConnectivity/obj/Debug/net10.0/GraphConnectivity.AssemblyInfo.cs
+++ GraphConnectivity/obj/Debug/net10.0/GraphConnectivity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphConnectivity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d248a84cf01fee0db76efed06988dfdae01ba16a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphConnectivity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphConnectivity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
